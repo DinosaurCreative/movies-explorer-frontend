73bec01801f8de4eebfb79d42d269158ac6b8ca7
German <gimalaevgerman@yandex.ru>
2021-09-21 00:20:26 +0300

    feat: create navigation layout and css 1280px

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Navigation from '../Navigation/Navigation';
 import './Header.css';
 
 function Header() {
@@ -8,12 +9,11 @@ function Header() {
       <div className="header__container">
       <Link to="/" href="#" className="logo logo_place_header" />
       <div className="header__links-container">
-        {/* <Link to="/movies" href="#" className="link" >{"Фильмы"}</Link>
-        <Link to="/saved-movies" href="#" className="link" >{"Сохраненные фильмы"}</Link> */}
-        <Link to="/signup" className="link link_header-registration" >{"Регистрация"}</Link>
-        <Link to="/signin" className="link link_header-authorization" >{"Войти"}</Link>
+        <Navigation />
+        {false && <Link to="/signup" className="link link_header-registration" >{"Регистрация"}</Link>}
+        {false && <Link to="/signin" className="link link_header-authorization" >{"Войти"}</Link>}
       </div>
-      {/* <Link to="/saved-movies" href="#" className="header__account-btn" /> */}
+      <Link to="/saved-movies" href="#" className="header__account-btn" />
       </div>
     </header>
   );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './Navigation.css';
+import { Link } from 'react-router-dom';
 
 function Navigation() {
-
+  return (
+    <div className='navigation'>
+      <Link to="/movies" href="#" className="link link_navigation" >{"Фильмы"}</Link>
+      <Link to="/saved-movies" href="#" className="link link_navigation" >{"Сохраненные фильмы"}</Link>
+    </div>
+  )
 }
 
 export default Navigation;
