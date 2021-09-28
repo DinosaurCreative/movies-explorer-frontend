e395b7f7d7cb6d56308f0bdd27dce11857e1df8b
German <gimalaevgerman@yandex.ru>
2021-09-28 22:31:55 +0300

    feat: make card and carlist 320 px layout

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -3,8 +3,10 @@ import middleofnowhere from '../../images/middleofnowhere.jpg';
 import stones from '../../images/stones.jpg';
 import eveningfrance from '../../images/eveningfrance.jpg';
 import sameplace from '../../images/sameplace.jpg';
-
+import { useLocation } from 'react-router-dom';
 function MoviesCardList() {
+  const location  = useLocation();
+
   return (
     <div className='movies-card-list'>
       <ul className='movie-card-list__catalog'>
@@ -33,7 +35,7 @@ function MoviesCardList() {
         <MovieCard card = {sameplace}
         />
       </ul>
-      <button className='movies-card-list__more-btn'>Ещё</button>
+      {location.pathname !== '/saved-movies' &&<button className='movies-card-list__more-btn'>Ещё</button>}
     </div>
   )
 }
